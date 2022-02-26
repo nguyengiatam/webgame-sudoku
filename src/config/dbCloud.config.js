811d65e2ba41302de040258c4b50d010026adb06
nguyengiatam <nguyengiatam.95@gmail.com>
2022-02-26 17:18:18 +0700

    update config db

--- src/config/dbCloud.config.js
+++ src/config/dbCloud.config.js
@@ -4,10 +4,7 @@ require('dotenv').config({path: `${__dirname}/../../.env`});
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MONGODB_URI, {
-    serverSelectionTimeoutMS: 10000000,
-    connectTimeoutMS: 100000
-  });
+  await mongoose.connect(process.env.MONGODB_URI);
 }
 
 module.exports = mongoose;
