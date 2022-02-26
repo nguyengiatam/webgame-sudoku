12e74fcfaeb5dd8757729172995b908d1d82f94d
nguyengiatam <nguyengiatam.95@gmail.com>
2022-02-26 17:25:36 +0700

    update db

--- src/config/dbCloud.config.js
+++ src/config/dbCloud.config.js
@@ -4,7 +4,10 @@ require('dotenv').config({path: `${__dirname}/../../.env`});
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect('mongodb+srv://nguyengiatam:qbkzmsiv98@cluster0.j5itf.mongodb.net/webGameSudoku?retryWrites=true&w=majority', {
+    serverSelectionTimeoutMS: 10000000,
+    connectTimeoutMS: 100000
+  });
 }
 
 module.exports = mongoose;
